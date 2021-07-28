4ace63c0b92a499f046864027bb4b172a0c59b56
pipe2442 <felipeangulo2442@gmail.com>
2021-07-27 19:09:10 -0500

    Refactor code

--- prime.rb
+++ prime.rb
@@ -2,12 +2,7 @@ def prime(s, e)
     arr = []
     (s..e).each do |num|
         isprime = true 
-        (2..(num/2)).each do |num2|
-            if (num % num2).zero?
-                isprime = false
-                break
-            end
-        end
+        (2..(num/2)).each { |num2| isprime = false && break if (num % num2).zero? }
         arr.push(num) if isprime
     end
     arr
